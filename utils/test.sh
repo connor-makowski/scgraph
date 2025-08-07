e6460d4a3d595c245748fb5ccab929255f1b8713
Connor Makowski <connorman528@gmail.com>
2025-08-07 14:10:58 -0400

    Start tracking test_output in git.

--- utils/test.sh
+++ utils/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 python --version
+rm -rf ./utils/test_output.txt
+touch ./utils/test_output.txt
 for file in /app/test/*.py; do
     [ -e "$file" ] || continue  # Skip if no files match
-    python "$file"
+    # Run the files, print the output and capture the output as well
+    python "$file" | tee -a ./utils/test_output.txt
 done
